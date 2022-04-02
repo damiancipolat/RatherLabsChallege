54042b0265322ba3c037071a0c45255bc89979a4
damiancipolat <damian.cipolat@gmail.com>
2022-04-02 16:21:01 -0300

    lint:fix lint problems

--- src/consumer/biz/bidask.test.ts
+++ src/consumer/biz/bidask.test.ts
@@ -1,13 +1,12 @@
 import {
-    bidAskIncrease,
-    bidAskDecrease,
-    bidAskLogic
-} from '../biz/bidasks';
+  bidAskIncrease,
+  bidAskDecrease,
+  bidAskLogic,
+} from './bidasks';
 
 import book from '../../store/book';
-  
+
 describe('Biz bidask test', () => {
-  
   beforeEach(() => {
     jest.clearAllMocks();
     jest.resetModules();
@@ -17,136 +16,122 @@ describe('Biz bidask test', () => {
     jest.clearAllMocks();
     jest.resetModules();
   });
-  
-  it('bid Increase', () => {    
-      
+
+  it('bid Increase', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem ={
-      PRICE:2000,
-      COUNT:10,
-      AMOUNT:100
+
+    const bookItem = {
+      PRICE: 2000,
+      COUNT: 10,
+      AMOUNT: 100,
     };
 
-    bidAskIncrease(bookItem,'ARS_USD');
-    const items =book.get('ARS_USD').bids;
+    bidAskIncrease(bookItem, 'ARS_USD');
+    const items = book.get('ARS_USD').bids;
 
     expect(items).toStrictEqual([bookItem]);
-
   });
 
-  it('ask Increase', () => {    
-      
+  it('ask Increase', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem ={
-      PRICE:2000,
-      COUNT:10,
-      AMOUNT:-1
+
+    const bookItem = {
+      PRICE: 2000,
+      COUNT: 10,
+      AMOUNT: -1,
     };
 
-    bidAskIncrease(bookItem,'ARS_USD');
-    const items =book.get('ARS_USD').asks;
+    bidAskIncrease(bookItem, 'ARS_USD');
+    const items = book.get('ARS_USD').asks;
 
     expect(items).toStrictEqual([bookItem]);
+  });
 
-  });  
-  
-  it('bid Decrease', () => {    
-      
+  it('bid Decrease', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem1 ={
-      PRICE:3000,
-      COUNT:10,
-      AMOUNT:1
+
+    const bookItem1 = {
+      PRICE: 3000,
+      COUNT: 10,
+      AMOUNT: 1,
     };
 
-    const bookItem2 ={
-      PRICE:3000,
-      COUNT:10,
-      AMOUNT:1
-    };    
+    const bookItem2 = {
+      PRICE: 3000,
+      COUNT: 10,
+      AMOUNT: 1,
+    };
 
-    //Record new bid/ask.
-    bidAskIncrease(bookItem1,'ARS_USD');
-    bidAskIncrease(bookItem2,'ARS_USD');
+    // Record new bid/ask.
+    bidAskIncrease(bookItem1, 'ARS_USD');
+    bidAskIncrease(bookItem2, 'ARS_USD');
 
-    //Decrease bid.
-    bidAskDecrease(bookItem2,'ARS_USD');
-    const items =book.get('ARS_USD').bids;
+    // Decrease bid.
+    bidAskDecrease(bookItem2, 'ARS_USD');
+    const items = book.get('ARS_USD').bids;
 
     expect(items).toStrictEqual([bookItem1]);
-
   });
 
-  it('ask Decrease', () => {    
-      
+  it('ask Decrease', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem1 ={
-      PRICE:3000,
-      COUNT:10,
-      AMOUNT:-1
+
+    const bookItem1 = {
+      PRICE: 3000,
+      COUNT: 10,
+      AMOUNT: -1,
     };
 
-    const bookItem2 ={
-      PRICE:3000,
-      COUNT:10,
-      AMOUNT:-1
-    };    
+    const bookItem2 = {
+      PRICE: 3000,
+      COUNT: 10,
+      AMOUNT: -1,
+    };
 
-    //Record new bid/ask.
-    bidAskIncrease(bookItem1,'ARS_USD');
-    bidAskIncrease(bookItem2,'ARS_USD');
+    // Record new bid/ask.
+    bidAskIncrease(bookItem1, 'ARS_USD');
+    bidAskIncrease(bookItem2, 'ARS_USD');
 
-    //Decrease bid.
-    bidAskDecrease(bookItem2,'ARS_USD');
-    const items =book.get('ARS_USD').asks;
+    // Decrease bid.
+    bidAskDecrease(bookItem2, 'ARS_USD');
+    const items = book.get('ARS_USD').asks;
 
     expect(items).toStrictEqual([bookItem1]);
-
   });
 
-  it('bidAskLogic Increase', () => {    
-      
+  it('bidAskLogic Increase', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem ={
-      PRICE:2000,
-      COUNT:1,
-      AMOUNT:100
+
+    const bookItem = {
+      PRICE: 2000,
+      COUNT: 1,
+      AMOUNT: 100,
     };
 
-    bidAskLogic(bookItem,'ARS_USD');
-    const items = book.get('ARS_USD').bids;    
+    bidAskLogic(bookItem, 'ARS_USD');
+    const items = book.get('ARS_USD').bids;
 
     expect(items).toStrictEqual([bookItem]);
-
   });
 
-  it('bidAskLogic Decrease', () => {    
-      
+  it('bidAskLogic Decrease', () => {
     book.clear();
     book.fill(['ARS_USD']);
-    
-    const bookItem ={
-      PRICE:2000,
-      COUNT:0,
-      AMOUNT:100
+
+    const bookItem = {
+      PRICE: 2000,
+      COUNT: 0,
+      AMOUNT: 100,
     };
 
-    bidAskLogic(bookItem,'ARS_USD');
-    const items = book.get('ARS_USD').bids;    
+    bidAskLogic(bookItem, 'ARS_USD');
+    const items = book.get('ARS_USD').bids;
 
     expect(items).toStrictEqual([]);
-
   });
-
 });
-  
