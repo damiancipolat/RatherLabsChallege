bc3028f2fae79bf94761255de03c572ff25e7184
damiancipolat <damian.cipolat@gmail.com>
2022-04-02 13:59:22 -0300

    add jest and base test

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+describe("test add function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(10+5).toBe(15);
+  });
+it("should return 5 for add(2,3)", () => {
+    expect(2+3).toBe(5);
+  });
+});
