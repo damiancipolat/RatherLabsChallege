d49bf1fd6e088476f7197c66890d73fd7efa9826
damiancipolat <damian.cipolat@gmail.com>
2022-04-03 22:21:39 -0300

    remove build

--- build/api/controller/health.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const health = (req, res) => {
-    res.status(200).json({ health: 'OK' });
-};
-exports.default = health;
